bed8a4584c0312093ad8f1ba89e4c033b5db3f87
SuperSpaceMC <unconfigured@null.spigotmc.org>
2021-02-03 21:01:27 +0400

    lone update

---
+++ src/main/java/net/mcreator/sotmr/block/LoneGrassBlockBlock.java
@@ -0,0 +1,88 @@
+
+package net.mcreator.sotmr.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.biome.BiomeColors;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.GrassColors;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.sotmr.itemgroup.SOTMBlocksItemGroup;
+import net.mcreator.sotmr.SotmModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@SotmModElements.ModElement.Tag
+public class LoneGrassBlockBlock extends SotmModElements.ModElement {
+	@ObjectHolder("sotm:lone_grass_block")
+	public static final Block block = null;
+	public LoneGrassBlockBlock(SotmModElements instance) {
+		super(instance, 2197);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new BlockColorRegisterHandler());
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(SOTMBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	private static class BlockColorRegisterHandler {
+		@OnlyIn(Dist.CLIENT)
+		@SubscribeEvent
+		public void blockColorLoad(ColorHandlerEvent.Block event) {
+			event.getBlockColors().register((bs, world, pos, index) -> {
+				return world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.get(0.5D, 1.0D);
+			}, block);
+		}
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ORGANIC).sound(SoundType.PLANT).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
+					.harvestLevel(0).harvestTool(ToolType.SHOVEL));
+			setRegistryName("lone_grass_block");
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side) {
+			return adjacentBlockState.getBlock() == this ? true : super.isSideInvisible(state, adjacentBlockState, side);
+		}
+
+		@Override
+		public MaterialColor getMaterialColor() {
+			return MaterialColor.GRASS;
+		}
+
+		@Override
+		public boolean canSustainPlant(BlockState state, IBlockReader world, BlockPos pos, Direction direction, IPlantable plantable) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(LoneDirtBlock.block, (int) (1)));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/sotmr/block/MonochromeWoodPlanksBlock.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.sotmr.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.sotmr.itemgroup.ToolsAndWeaponsItemGroup;
+import net.mcreator.sotmr.SotmModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@SotmModElements.ModElement.Tag
+public class MonochromeWoodPlanksBlock extends SotmModElements.ModElement {
+	@ObjectHolder("sotm:monochrome_wood_planks")
+	public static final Block block = null;
+	public MonochromeWoodPlanksBlock(SotmModElements instance) {
+		super(instance, 2200);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ToolsAndWeaponsItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 3f).setLightLevel(s -> 0).harvestLevel(0)
+					.harvestTool(ToolType.AXE));
+			setRegistryName("monochrome_wood_planks");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 5;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
