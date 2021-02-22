44fa77499c0fdbe579b72d8fb767e644f1223dec
NovaWolfGaming <owner@DESKTOP-BKA6O3P.lan>
2021-02-22 17:21:38 +0000

    Fixed Crolphin

--- src/main/java/net/mcreator/sotmr/block/MonochromeWoodLogBlock.java
+++ src/main/java/net/mcreator/sotmr/block/MonochromeWoodLogBlock.java
@@ -1,17 +1,34 @@
 
 package net.mcreator.sotmr.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.common.IPlantable;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.sotmr.itemgroup.SOTMBlocksItemGroup;
+import net.mcreator.sotmr.SotmModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @SotmModElements.ModElement.Tag
 public class MonochromeWoodLogBlock extends SotmModElements.ModElement {
-
 	@ObjectHolder("sotm:monochrome_wood_log")
 	public static final Block block = null;
-
 	public MonochromeWoodLogBlock(SotmModElements instance) {
 		super(instance, 2199);
-
 	}
 
 	@Override
@@ -19,15 +36,10 @@ public class MonochromeWoodLogBlock extends SotmModElements.ModElement {
 		elements.blocks.add(() -> new CustomBlock());
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(SOTMBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 2f).setLightLevel(s -> 0).harvestLevel(0)
-							.harvestTool(ToolType.AXE));
-
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 2f).setLightLevel(s -> 0).harvestLevel(0)
+					.harvestTool(ToolType.AXE));
 			setRegistryName("monochrome_wood_log");
 		}
 
@@ -43,13 +55,10 @@ public class MonochromeWoodLogBlock extends SotmModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
