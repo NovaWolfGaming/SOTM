fafa940970e70378b0b290d0458c8037e24eecaf
NovaWolfGaming <owner@DESKTOP-BKA6O3P.lan>
2021-04-17 20:02:54 +0100

    Fixed a few broken textures

--- src/main/java/net/mcreator/sotmr/block/SiltBlock.java
+++ src/main/java/net/mcreator/sotmr/block/SiltBlock.java
@@ -1,17 +1,30 @@
 
 package net.mcreator.sotmr.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.sotmr.itemgroup.SOTMBlocksItemGroup;
+import net.mcreator.sotmr.SotmModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @SotmModElements.ModElement.Tag
 public class SiltBlock extends SotmModElements.ModElement {
-
 	@ObjectHolder("sotm:silt")
 	public static final Block block = null;
-
 	public SiltBlock(SotmModElements instance) {
 		super(instance, 2356);
-
 	}
 
 	@Override
@@ -19,27 +32,19 @@ public class SiltBlock extends SotmModElements.ModElement {
 		elements.blocks.add(() -> new CustomBlock());
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(SOTMBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
-							.harvestLevel(1).harvestTool(ToolType.SHOVEL).setRequiresTool());
-
+			super(Block.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.SHOVEL).setRequiresTool());
 			setRegistryName("silt");
 		}
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/sotmr/procedures/ToxicBricksOnBlockRightClickedProcedure.java
+++ src/main/java/net/mcreator/sotmr/procedures/ToxicBricksOnBlockRightClickedProcedure.java
@@ -1,11 +1,19 @@
 package net.mcreator.sotmr.procedures;
 
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.sotmr.SotmModElements;
+import net.mcreator.sotmr.SotmMod;
+
+import java.util.Map;
+
 @SotmModElements.ModElement.Tag
 public class ToxicBricksOnBlockRightClickedProcedure extends SotmModElements.ModElement {
-
 	public ToxicBricksOnBlockRightClickedProcedure(SotmModElements instance) {
 		super(instance, 2337);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -14,12 +22,8 @@ public class ToxicBricksOnBlockRightClickedProcedure extends SotmModElements.Mod
 				SotmMod.LOGGER.warn("Failed to load dependency entity for procedure ToxicBricksOnBlockRightClicked!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.WITHER, (int) 200, (int) 1, (true), (false)));
-
 	}
-
 }
